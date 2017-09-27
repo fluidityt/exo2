dc1502188eacc697770ad9a51729cc2ca29c25ef
fluidityt <hackbook17@justins-macbook-pro.local>
2017-09-27 13:26:38 -0400

    ffs

--- exo2/GameScene.swift
+++ exo2/GameScene.swift
@@ -9,102 +9,107 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
-    
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
+var gScene = GameScene()
 
-        self.lastUpdateTime = 0
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+fileprivate enum MovingDirection: String { case up, down, left, right }
+
+class Platforms_BoxPathComponent: GKComponent {
+  
+  private var node: SKSpriteNode!
+  
+  private var state: MovingDirection = .right
+  
+  @GKInspectable var startingDirection: String = "down"
+  
+  @GKInspectable var uniqueName: String = "default"
+  
+  @GKInspectable var xPathSize: CGFloat = 200
+  
+  @GKInspectable var yPathSize: CGFloat = 400
+  
+  private lazy var startingPos: CGPoint = { self.node.position }()
+  
+  // Start at top right, moving clockwise:
+  private var isTooFarRight: Bool { return self.node.position.x > (self.startingPos.x + self.xPathSize) }
+  private var isTooFarDown: Bool { return self.node.position.y < (self.startingPos.y - self.yPathSize) }
+  private var isTooFarLeft: Bool { return self.node.position.x < self.startingPos.x }
+  private var isTooFarUp: Bool { return self.node.position.y > self.startingPos.y }
+  
+  override func didAddToEntity() {
+    print("adding component")
+    // can't add node here because nodes aren't part of scene yet :(
+    // possibly do a thread?
+  }
+  
+  override func update(deltaTime seconds: TimeInterval) {
+    if node == nil { node = gScene.childNode(withName: uniqueName) as! SKSpriteNode }
+    
+    let amount: CGFloat = 1
+    
+    // Start at top left, moving clockwise:
+    switch state {
+    
+    case .up:
+      if isTooFarUp {
+        state = .right
+        fallthrough
+      } else { node.position.y += amount }
+    
+    case .right:
+      if isTooFarRight {
+        state = .down
+        fallthrough
+      } else { node.position.x += amount }
+    
+    case .down:
+      if isTooFarDown {
+        state = .left
+        fallthrough
+      } else { node.position.y -= amount }
+    
+    case .left:
+      if isTooFarLeft {
+        state = .up
+        fallthrough
+      } else { node.position.x -= amount }
+    
+    default:
+      print("and xcode said i will never be exectured")
+      node.position.y += amount
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+  }
+  
+}
+
+class GameScene: SKScene {
+
+  override func didMove(to view: SKView) {
+    gScene = self
+  }
+  
+  var entities = [GKEntity]()
+  var graphs = [String : GKGraph]()
+  
+  private var lastUpdateTime : TimeInterval = 0
+  
+  
+  func gkUpdate(_ currentTime: TimeInterval) -> TimeInterval {
+    if (self.lastUpdateTime == 0) {
+      self.lastUpdateTime = currentTime
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+    let dt = currentTime - self.lastUpdateTime
+    
+    for entity in self.entities {
+      entity.update(deltaTime: dt)
     }
     
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
+    return currentTime
+  }
+  
+  override func update(_ currentTime: TimeInterval) {
+    self.lastUpdateTime = gkUpdate(currentTime)
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
-    }
+  }
 }
--- exo2/GameViewController.swift
+++ exo2/GameViewController.swift
@@ -63,3 +63,121 @@ class GameViewController: UIViewController {
         return true
     }
 }
+
+/*
+ 
+ //
+ //  GameScene.swift
+ //  exo2
+ //
+ //  Created by justin fluidity on 9/27/17.
+ //  Copyright © 2017 justin fluidity. All rights reserved.
+ //
+ 
+ import SpriteKit
+ import GameplayKit
+ 
+ var gScene = GameScene()
+ 
+ fileprivate enum MovingDirection: String { case up, down, left, right }
+ 
+ class Platforms_BoxPathComponent: GKComponent {
+ 
+ private var node: SKSpriteNode!
+ 
+ private var state: MovingDirection = .up
+ 
+ @GKInspectable var startingDirection: String = "down"
+ 
+ @GKInspectable var uniqueName: String = "default"
+ 
+ @GKInspectable var xPathSize: CGFloat = 200
+ 
+ @GKInspectable var yPathSize: CGFloat = 400
+ 
+ private lazy var startingPos: CGPoint = { self.node.position }()
+ 
+ // Start at top right, moving clockwise:
+ // private var isTooFarRight: Bool { return self.node.position.x > (self.startingPos.x + self.xPathSize) }
+ private var isTooFarDown: Bool { return self.node.position.y < (self.startingPos.y - self.yPathSize) }
+ //private var isTooFarLeft: Bool { return self.node.position.x < self.startingPos.x }
+ private var isTooFarUp: Bool { return self.node.position.y > self.startingPos.y }
+ 
+ override func didAddToEntity() {
+ print("adding component")
+ // can't add node here because nodes aren't part of scene yet :(
+ // possibly do a thread?
+ }
+ 
+ override func update(deltaTime seconds: TimeInterval) {
+ if node == nil { node = gScene.childNode(withName: uniqueName) as! SKSpriteNode }
+ 
+ let amount: CGFloat = 1
+ 
+ // Start at top left, moving clockwise:
+ switch state {
+ 
+ case .up:
+ if isTooFarUp {
+ state = .down //.right
+ fallthrough
+ } else { node.position.y += amount }
+ /*
+ case .right:
+ if isTooFarRight {
+ state = .down
+ fallthrough
+ } else { node.position.x += amount }
+ */
+ case .down:
+ if isTooFarDown {
+ state = .up //.left
+ fallthrough
+ } else { node.position.y -= amount }
+ /*
+ case .left:
+ if isTooFarLeft {
+ state = .up
+ // goto: start (lol)
+ } else { node.position.x -= amount }
+ */
+ default: node.position.y += amount
+ }
+ 
+ }
+ 
+ }
+ 
+ class GameScene: SKScene {
+ 
+ override func didMove(to view: SKView) {
+ gScene = self
+ }
+ 
+ var entities = [GKEntity]()
+ var graphs = [String : GKGraph]()
+ 
+ private var lastUpdateTime : TimeInterval = 0
+ 
+ 
+ func gkUpdate(_ currentTime: TimeInterval) -> TimeInterval {
+ if (self.lastUpdateTime == 0) {
+ self.lastUpdateTime = currentTime
+ }
+ 
+ let dt = currentTime - self.lastUpdateTime
+ 
+ for entity in self.entities {
+ entity.update(deltaTime: dt)
+ }
+ 
+ return currentTime
+ }
+ 
+ override func update(_ currentTime: TimeInterval) {
+ self.lastUpdateTime = gkUpdate(currentTime)
+ 
+ }
+ }
+
+ */
